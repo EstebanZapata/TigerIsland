26ced5a89f6aed66ba08af748846f83f4d0746cb
Lucas Bassett-Audain <lbassettaudain@ufl.edu>
2017-03-23 21:41:32 -0400

    ACCEPTANCE: Add volcano habitability test

---
+++ test/acceptance/Habitability.java
@@ -0,0 +1,31 @@
+package acceptance;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import org.junit.Assert;
+import tile.Hex;
+import tile.Terrain;
+import tile.Tile;
+
+public class Habitability {
+    private Tile myTile;
+    private boolean isHabitable;
+    @Given("^I have a tile$")
+    public void i_have_a_tile() throws Throwable {
+        this.myTile = new Tile(Terrain.JUNGLE, Terrain.LAKE );
+    }
+
+    @When("^I check if a volcano is habitable$")
+    public void i_check_if_a_volcano_is_habitable() throws Throwable {
+        Hex myVolcano = myTile.getVolcanoHex();
+        this.isHabitable = myVolcano.getTerrain().isHabitable();
+    }
+
+    @Then("^it is not habitable$")
+    public void it_is_not_habitable() throws Throwable {
+        Assert.assertEquals(false, this.isHabitable);
+    }
+
+}
