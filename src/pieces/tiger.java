69e31c9ab6ea4d4eb575f6e7498e30e642b8bd1c
Jenna Kovacs <jkovacs@ufl.edu>
2017-03-24 11:29:54 -0400

    Jenna's first commit

--- src/pieces/tiger.java
+++ src/pieces/tiger.java
@@ -1,7 +1,49 @@
 package pieces;
 
-/**
- * Created by Jenna on 3/23/2017.
- */
+import tile.Location;
+
 public class tiger {
+    private int tigerID;
+    private String color;
+    private Location location;
+    private String status;  // in_play or not_yet_played or killed
+
+    public tiger(int tigerID, String color, Location location, String status) {
+        this.tigerID = tigerID;
+        this.color = color;
+        this.location = location;
+        this.status = status;
+    }
+
+    public int getTigerID() {
+        return tigerID;
+    }
+
+    public void setTigerID(int tigerID) {
+        this.tigerID = tigerID;
+    }
+
+    public String getTigerColor() {
+        return color;
+    }
+
+    public void setTigerColor(String color) {
+        this.color = color;
+    }
+
+    public Location getTigerLocation() {
+        return location;
+    }
+
+    public void setTigerLocation(Location location) {
+        this.location = location;
+    }
+
+    public String getTigerStatus() {
+        return status;
+    }
+
+    public void setTigerStatus(String status) {
+        this.status = status;
+    }
 }
