81d86b95dc73f73af79f53752b142781873c3313
Esteban Zapata <zapata.e96@gmail.com>
2017-04-06 16:57:49 -0400

    Tests for CoordinateSystemHelper

--- src/game/world/CoordinateSystemHelper.java
+++ src/game/world/CoordinateSystemHelper.java
@@ -4,82 +4,22 @@ import tile.Location;
 import tile.orientation.HexOrientation;
 import tile.orientation.TileOrientation;
 
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class CoordinateSystemHelper {
 
-    public static Location getHexLocationRelativeToOrientationAndCenter(Location center, HexOrientation hexOrientation) {
-        Location[] adjacentHexLocations = getHexLocationsAdjacentToCenter(center);
+    public static Location getTentativeLeftHexLocation(Location locationOfVolcano, TileOrientation tileOrientation) {
+        HexOrientation leftHexOrientation = getLeftHexOrientationFromTileOrientation(tileOrientation);
 
-        return adjacentHexLocations[hexOrientation.getOrientationInteger()];
+        return getHexLocationRelativeToOrientationAndCenter(locationOfVolcano, leftHexOrientation);
     }
 
-    public static Location[] getHexLocationsAdjacentToCenter(Location center) {
-        Location[] adjacentLocations = new Location[6];
+    public static Location getTentativeRightHexLocation(Location locationOfVolcano, TileOrientation tileOrientation) {
+        HexOrientation rightHexOrientation = CoordinateSystemHelper.getRightHexOrientationFromTileOrientation(tileOrientation);
 
-        int xCoordinateOfCenter = center.getxCoordinate();
-        int yCoordinateOfCenter = center.getyCoordinate();
-        int zCoordinateOfCenter = center.getzCoordinate();
-
-
-        adjacentLocations[HexOrientation.SOUTHWEST.getOrientationInteger()] =
-                new Location(xCoordinateOfCenter - 1, yCoordinateOfCenter - 1, zCoordinateOfCenter);
-        adjacentLocations[HexOrientation.WEST.getOrientationInteger()] =
-                new Location(xCoordinateOfCenter - 1, yCoordinateOfCenter, zCoordinateOfCenter);
-        adjacentLocations[HexOrientation.NORTHWEST.getOrientationInteger()] =
-                new Location(xCoordinateOfCenter, yCoordinateOfCenter + 1, zCoordinateOfCenter);
-        adjacentLocations[HexOrientation.NORTHEAST.getOrientationInteger()] =
-                new Location(xCoordinateOfCenter + 1, yCoordinateOfCenter + 1, zCoordinateOfCenter);
-        adjacentLocations[HexOrientation.EAST.getOrientationInteger()] =
-                new Location(xCoordinateOfCenter + 1, yCoordinateOfCenter, zCoordinateOfCenter);
-        adjacentLocations[HexOrientation.SOUTHEAST.getOrientationInteger()] =
-                new Location(xCoordinateOfCenter, yCoordinateOfCenter - 1, zCoordinateOfCenter);
-
-        return adjacentLocations;
-
-    }
-
-    public static Location[] getAdjacentHexLocationsToTile(Location[] hexLocations) {
-        ArrayList<Location> adjacentLocations = new ArrayList<>();
-
-        Location[] locationsAdjacentToVolcano = getHexLocationsAdjacentToCenter(hexLocations[0]);
-        Location[] locationsAdjacentToLeftHex = getHexLocationsAdjacentToCenter(hexLocations[1]);
-        Location[] locationsAdjacentToRightHex = getHexLocationsAdjacentToCenter(hexLocations[2]);
-
-        for(Location locationAdjacentToVolcano:locationsAdjacentToVolcano) {
-            adjacentLocations.add(locationAdjacentToVolcano);
-        }
-
-        insertNonRepeatingLocationsIntoAdjacentLocations(adjacentLocations, locationsAdjacentToLeftHex);
-        insertNonRepeatingLocationsIntoAdjacentLocations(adjacentLocations, locationsAdjacentToRightHex);
-
-        return adjacentLocations.toArray(new Location[adjacentLocations.size()]);
-
-
-    }
-
-    private static void insertNonRepeatingLocationsIntoAdjacentLocations(ArrayList<Location> adjacentLocations, Location[] locationsAdjacentToHex) {
-        for (Location locationAdjacentToLeftHex:locationsAdjacentToHex) {
-            for(int i = 0; i < adjacentLocations.size(); i++) {
-                Location adjacentLocation = adjacentLocations.get(i);
-
-                if (locationsAreNotEqual(locationAdjacentToLeftHex, adjacentLocation)) {
-                    adjacentLocations.add(locationAdjacentToLeftHex);
-                }
-            }
-        }
-    }
-
-    private static boolean locationsAreNotEqual(Location locationAdjacentToLeftHex, Location locationAdjacentToVolcano) {
-        int x1 = locationAdjacentToLeftHex.getxCoordinate();
-        int y1 = locationAdjacentToLeftHex.getyCoordinate();
-        int z1 = locationAdjacentToLeftHex.getzCoordinate();
-
-        int x2 = locationAdjacentToVolcano.getxCoordinate();
-        int y2 = locationAdjacentToVolcano.getyCoordinate();
-        int z2 = locationAdjacentToVolcano.getzCoordinate();
-
-        return !(x1==x2 && y1==y2 && z1==z2);
+        return getHexLocationRelativeToOrientationAndCenter(locationOfVolcano, rightHexOrientation);
     }
 
     public static HexOrientation getLeftHexOrientationFromTileOrientation(TileOrientation tileOrientation) {
@@ -146,15 +86,64 @@ public class CoordinateSystemHelper {
         return hexOrientation;
     }
 
-    public static Location getTentativeLeftHexLocation(Location locationOfVolcano, TileOrientation tileOrientation) {
-        HexOrientation leftHexOrientation = getLeftHexOrientationFromTileOrientation(tileOrientation);
 
-        return getHexLocationRelativeToOrientationAndCenter(locationOfVolcano, leftHexOrientation);
+    public static Location getHexLocationRelativeToOrientationAndCenter(Location center, HexOrientation hexOrientation) {
+        Location[] adjacentHexLocations = getHexLocationsAdjacentToCenter(center);
+
+        return adjacentHexLocations[hexOrientation.getOrientationInteger()];
     }
 
-    public static Location getTentativeRightHexLocation(Location locationOfVolcano, TileOrientation tileOrientation) {
-        HexOrientation rightHexOrientation = CoordinateSystemHelper.getRightHexOrientationFromTileOrientation(tileOrientation);
+    public static Location[] getHexLocationsAdjacentToCenter(Location center) {
+        Location[] adjacentLocations = new Location[6];
 
-        return getHexLocationRelativeToOrientationAndCenter(locationOfVolcano, rightHexOrientation);
+        int xCoordinateOfCenter = center.getxCoordinate();
+        int yCoordinateOfCenter = center.getyCoordinate();
+        int zCoordinateOfCenter = center.getzCoordinate();
+
+
+        adjacentLocations[HexOrientation.SOUTHWEST.getOrientationInteger()] =
+                new Location(xCoordinateOfCenter - 1, yCoordinateOfCenter - 1, zCoordinateOfCenter);
+        adjacentLocations[HexOrientation.WEST.getOrientationInteger()] =
+                new Location(xCoordinateOfCenter - 1, yCoordinateOfCenter, zCoordinateOfCenter);
+        adjacentLocations[HexOrientation.NORTHWEST.getOrientationInteger()] =
+                new Location(xCoordinateOfCenter, yCoordinateOfCenter + 1, zCoordinateOfCenter);
+        adjacentLocations[HexOrientation.NORTHEAST.getOrientationInteger()] =
+                new Location(xCoordinateOfCenter + 1, yCoordinateOfCenter + 1, zCoordinateOfCenter);
+        adjacentLocations[HexOrientation.EAST.getOrientationInteger()] =
+                new Location(xCoordinateOfCenter + 1, yCoordinateOfCenter, zCoordinateOfCenter);
+        adjacentLocations[HexOrientation.SOUTHEAST.getOrientationInteger()] =
+                new Location(xCoordinateOfCenter, yCoordinateOfCenter - 1, zCoordinateOfCenter);
+
+        return adjacentLocations;
+
     }
+
+    public static Location[] getAdjacentHexLocationsToTile(Location[] hexLocations) {
+
+        Location[] locationsAdjacentToVolcano = getHexLocationsAdjacentToCenter(hexLocations[0]);
+        Location[] locationsAdjacentToLeftHex = getHexLocationsAdjacentToCenter(hexLocations[1]);
+        Location[] locationsAdjacentToRightHex = getHexLocationsAdjacentToCenter(hexLocations[2]);
+
+        Location[] adjacentLocationsToTile =
+                combineListsAndRemoveDuplicates(locationsAdjacentToVolcano, locationsAdjacentToLeftHex, locationsAdjacentToRightHex);
+
+        return adjacentLocationsToTile;
+
+    }
+
+    private static Location[] combineListsAndRemoveDuplicates(Location[] locationsOne, Location[] locationsTwo, Location[] locationsThree){
+        Set<Location> setWithoutDupes = new HashSet<>(Arrays.asList(locationsOne));
+        setWithoutDupes.addAll(Arrays.asList(locationsTwo));
+        setWithoutDupes.addAll(Arrays.asList(locationsThree));
+
+        return setWithoutDupes.toArray(new Location[0]);
+
+
+
+    }
+
+
+
+
+
 }
