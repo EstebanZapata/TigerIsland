43cb70dcf60247d1849010a817c737312aec1a2b
Liam <liamschmidt14@gmail.com>
2017-04-11 19:20:02 -0400

    Created functions to get a player's largest settlement and just the size of their largest settlement.

--- src/game/player/Player.java
+++ src/game/player/Player.java
@@ -122,4 +122,14 @@ public class Player {
             throw new BuildConditionsNotMetException(e.getMessage());
         }
     }
+
+    public int getSizeOfLargestSettlement() {
+        int sizeOfLargestSettlement = settlementManager.sizeOfLargestContainedSettlement();
+        return sizeOfLargestSettlement;
+    }
+
+    public Settlement getLargestSettlement() {
+        Settlement largestSettlement = settlementManager.getLargestContainedSettlement();
+        return largestSettlement;
+    }
 }
