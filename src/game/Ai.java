fce79cb04b23c5b4a1684cc79eb9b7e9508a315e
Lucas Bassett-Audain <lbassettaudain@ufl.edu>
2017-04-12 15:54:05 -0400

    AI: AI V2 add meeple counts and fix to actual build actions

--- src/game/Ai.java
+++ src/game/Ai.java
@@ -1,6 +1,7 @@
 package game;
 
 import game.player.Player;
+import game.settlements.BuildAction;
 import game.settlements.Settlement;
 import game.settlements.exceptions.NoPlayableHexException;
 import game.settlements.exceptions.NoSettlementOnHexException;
@@ -14,22 +15,30 @@ import game.world.rules.exceptions.AiDangerException;
 import game.world.rules.exceptions.IllegalTilePlacementException;
 import thread.message.GameActionMessage;
 
+import static game.settlements.BuildAction.BUILT_TOTORO_SANCTUARY;
 import static game.settlements.BuildAction.FOUNDED_SETTLEMENT;
+import static game.settlements.BuildAction.UNABLE_TO_BUILD;
 import static game.tile.Terrain.GRASSLANDS;
 import static game.tile.orientation.TileOrientation.NORTHEAST_NORTHWEST;
 import static game.tile.orientation.TileOrientation.NORTHWEST_WEST;
 
 public class Ai {
+    private int villagerCount;
+    private int totorroCount;
     private int settleCount;
     public Player p1;
     public Player p2;
     public World world;
     public Location previousBuildHex = null;
+    private BuildAction buildType;
+
     public Ai(World world, Player p1, Player p2){
         this.p1 = p1;
         this.p2 = p2;
         this.world = world;
         this.settleCount = 0;
+        this.totorroCount = 2;
+        this.villagerCount = 20;
     }
 
     public GameActionMessage chooseMove(String gameID, int moveNumber, String playerID, Tile tileToBePlaced) throws IllegalTilePlacementException {
@@ -46,43 +55,36 @@ public class Ai {
             boolean canBuildTotoro = false;
 
 
-            if(canBuildTotoro) {
-                //Build Totoro
-                previousBuildHex = null;
-                settleCount=0;
-                canBuildTotoro = false;
-            }
-            else {
-                if (previousBuildHex == null)
+
+                if (previousBuildHex == null) {
                     previousBuildHex = locationOnWhichToBuild;
+                    buildType = FOUNDED_SETTLEMENT;
+                    villagerCount--;
+                }
                 else if (settleCount < 3 && p1.settlementManager.getSettlementFromHex(world.getHexByCoordinate(previousBuildHex.getxCoordinate() - 2, previousBuildHex.getyCoordinate(), previousBuildHex.getHeight())) == null &&
                         p2.settlementManager.getSettlementFromHex(world.getHexByCoordinate(previousBuildHex.getxCoordinate() - 2, previousBuildHex.getyCoordinate(), previousBuildHex.getHeight())) == null) {
                     locationOnWhichToBuild = new Location(previousBuildHex.getxCoordinate() - 2, previousBuildHex.getyCoordinate(), 0);
+                    buildType = FOUNDED_SETTLEMENT;
+                    villagerCount--;
                 } else if (settleCount < 5 && settleCount >= 3 && p1.settlementManager.getSettlementFromHex(world.getHexByCoordinate(previousBuildHex.getxCoordinate() - 2, previousBuildHex.getyCoordinate(), previousBuildHex.getHeight())) == null &&
                         p2.settlementManager.getSettlementFromHex(world.getHexByCoordinate(previousBuildHex.getxCoordinate() - 2, previousBuildHex.getyCoordinate(), previousBuildHex.getHeight())) == null) {
                     locationOnWhichToBuild = new Location(previousBuildHex.getxCoordinate() + settleCount - 2, previousBuildHex.getyCoordinate() - settleCount + 2, 0);
-
-
+                    buildType = FOUNDED_SETTLEMENT;
+                    villagerCount--;
                 }else if(settleCount>=5){
                     settleCount = 0;
-                    if (playerID == p1.playerID) {
+                    if (playerID == p1.playerID&& totorroCount > 0) {
                         Location templocationOnWhichToBuild = new Location(previousBuildHex.getxCoordinate() + settleCount - 1, previousBuildHex.getyCoordinate() - settleCount + 1, 0);
                         try {
                             p1.settlementManager.buildTotoroSanctuary(world.getHexByLocation(locationOnWhichToBuild));
                             locationOnWhichToBuild = templocationOnWhichToBuild;
                             previousBuildHex = null;
+                            buildType = BUILT_TOTORO_SANCTUARY;
+                            totorroCount--;
+
                         }
                         catch (NoPlayableHexException error) {
-                            if (playerID == p2.playerID) {
-                                templocationOnWhichToBuild = new Location(previousBuildHex.getxCoordinate() + settleCount - 1, previousBuildHex.getyCoordinate() - settleCount + 1, 0);
-                                try {
-                                    p2.settlementManager.buildTotoroSanctuary(world.getHexByLocation(locationOnWhichToBuild));
-                                    locationOnWhichToBuild = templocationOnWhichToBuild;
-                                    previousBuildHex = null;
-                                } catch (NoPlayableHexException error2) {
 
-                                }
-                            }
                         }
                     }
                 }
@@ -92,12 +94,16 @@ public class Ai {
                 }
 
                 settleCount++;
-            }
+
             System.out.println(locationOfNewTile.getxCoordinate());
             System.out.println(locationOfNewTile.getyCoordinate());
             System.out.println(NORTHEAST_NORTHWEST);
             world.insertTileIntoTileManager(tileToBePlaced,locationOfNewTile,NORTHEAST_NORTHWEST);
-            return new GameActionMessage(gameID, moveNumber, playerID, tileToBePlaced, locationOfNewTile, NORTHEAST_NORTHWEST, FOUNDED_SETTLEMENT, locationOnWhichToBuild, GRASSLANDS);
+            if(villagerCount<0){
+                return new GameActionMessage(gameID, moveNumber, playerID, tileToBePlaced, locationOfNewTile, NORTHEAST_NORTHWEST, UNABLE_TO_BUILD, locationOnWhichToBuild, GRASSLANDS);
+
+            }
+            return new GameActionMessage(gameID, moveNumber, playerID, tileToBePlaced, locationOfNewTile, NORTHEAST_NORTHWEST, buildType, locationOnWhichToBuild, GRASSLANDS);
 
         }
         catch (AiDangerException error){
@@ -110,6 +116,11 @@ public class Ai {
             int buildYCoordinate = newTileYCoordinate;
             Location locationOnWhichToBuild = new Location(buildXCoordinate, buildYCoordinate, 0);
             world.insertTileIntoTileManager(tileToBePlaced,locationOfNewTile,NORTHWEST_WEST);
+            villagerCount--;
+            if(villagerCount<0){
+                return new GameActionMessage(gameID, moveNumber, playerID, tileToBePlaced, locationOfNewTile, NORTHEAST_NORTHWEST, UNABLE_TO_BUILD, locationOnWhichToBuild, GRASSLANDS);
+            }
+
             return new GameActionMessage(gameID, moveNumber, playerID, tileToBePlaced, locationOfNewTile, NORTHWEST_WEST, FOUNDED_SETTLEMENT, locationOnWhichToBuild, GRASSLANDS);
 
         } catch (NoSettlementOnHexException e) {
@@ -122,6 +133,10 @@ public class Ai {
             int buildYCoordinate = newTileYCoordinate;
             Location locationOnWhichToBuild = new Location(buildXCoordinate, buildYCoordinate, 0);
             world.insertTileIntoTileManager(tileToBePlaced,locationOfNewTile,NORTHWEST_WEST);
+            villagerCount--;
+            if(villagerCount<0){
+                return new GameActionMessage(gameID, moveNumber, playerID, tileToBePlaced, locationOfNewTile, NORTHEAST_NORTHWEST, UNABLE_TO_BUILD, locationOnWhichToBuild, GRASSLANDS);
+            }
             return new GameActionMessage(gameID, moveNumber, playerID, tileToBePlaced, locationOfNewTile, NORTHWEST_WEST, FOUNDED_SETTLEMENT, locationOnWhichToBuild, GRASSLANDS);
 
         }
